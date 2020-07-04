6272e54a7b1f3ba2ac3223c0e7fa618e0d337a17
Danny Lantigua <lantiguagrullon@gmail.com>
2020-07-04 10:34:05 -0400

    Added some java exercises and more OCA Notes

---
+++ OCA - OCP exercises/ImportExample.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class ImportExample {
+	public static void main(String... args) {
+		Random r = new Random();
+		//Print random number between 0 and 9
+		System.out.println(r.nextInt(10));
+	}
+}
