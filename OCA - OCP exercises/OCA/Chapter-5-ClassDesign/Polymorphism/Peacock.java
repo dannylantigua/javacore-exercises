f7db24ab80a788aa9b3abbc60abd5672a3a4d3cd
USGDAL <dlantigua@kemper.com>
2021-09-05 12:51:14 -0400

    Finished with polymorphism

---
+++ OCA - OCP exercises/OCA/Chapter-5-ClassDesign/Polymorphism/Peacock.java
@@ -0,0 +1,11 @@
+public class Peacock extends Bird {
+	
+	public String getName() {
+		return "Peacock";
+	}
+	
+	public static void main(String[] args) {
+		Bird bird = new Peacock();
+		bird.displayInformation();
+	}
+}
---
+++ OCA - OCP exercises/OCA/Chapter-5-ClassDesign/Polymorphism/ZooWorker.java
@@ -0,0 +1,12 @@
+public class ZooWorker {
+	
+	public static void feed(Reptile reptile) {
+		System.out.println("Feeding: " + reptile.getName());
+	}
+	
+	public static void main(String... args) {
+		feed(new Alligator());
+		feed(new Crocodile());
+		feed(new Reptile());
+	}
+}
